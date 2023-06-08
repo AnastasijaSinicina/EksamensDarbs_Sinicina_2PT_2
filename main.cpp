0de4dff239c293e917f58dd320714962feb6005e
AnastasijaSinicina <129928532+AnastasijaSinicina@users.noreply.github.com>
2023-06-08 18:06:31 +0000

    Tiek izveidoti 3 masīvi, kuros glabājas jautājumi, atbilžu varianti un atbildes, pievienots pirmais jautājums un atbildes uz to

--- main.cpp
+++ main.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
-
 int main() {
+using namespace std;
+  string jautajumi[10]; //masīvs ar jautājumiem
+  string atbilzuVar[10]; //masīvs ar atbilžu variantiem
+  string pareizAtbildes[10]; //masīvas ar pareizajām atbildēm
+
+  jautajumi [1] = "1. Kā saucās cikls kura pieraksts ir šāds?\n"
+    "    while(<nosacījums>){"
+    "    izpildāmie operatori; }\n";
+
+
+
+  pareizAtbildes[1] = "\tA. Cikls ar pēcnosacījumu\n"
+    "\tB. Cikls ar priekšnosacījumu\n"
+    "\tC. While\tD. Do...while\n";
+
+  pareizAtbildes[1] = "A,C";
   
 }
