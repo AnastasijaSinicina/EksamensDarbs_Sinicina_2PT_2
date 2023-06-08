0296cca63223d3e644306f965ae079b551094bcb
AnastasijaSinicina <129928532+AnastasijaSinicina@users.noreply.github.com>
2023-06-08 18:36:59 +0000

    Pievienots for cikls ar jautājumu un atbilžu variantu izvadi, pievienots otrais jautājums un atbildes

--- main.cpp
+++ main.cpp
@@ -8,13 +8,24 @@ using namespace std;
   jautajumi [1] = "1. Kā saucās cikls kura pieraksts ir šāds?\n"
     "    while(<nosacījums>){"
     "    izpildāmie operatori; }\n";
+  jautajumi[2] = "2. Kuri apgalvojumi apraksta while ciklu?";
 
 
 
-  pareizAtbildes[1] = "\tA. Cikls ar pēcnosacījumu\n"
+  atbilzuVar[1] = "\tA. Cikls ar pēcnosacījumu\n"
     "\tB. Cikls ar priekšnosacījumu\n"
-    "\tC. While\tD. Do...while\n";
+    "\tC. While\n\tD. Do...while\n";
+  atbilzuVar[2] = "\tA. Vispirms pārbaudi, pēc tam dari.\n"
+    "\tB. Konstrukcija ar nosacījumu cikla sākumā.\n"
+    "\tC. Konstrukcija ar nosacījumu cikla beigās.\n"
+    "\tD. Ja nosacījums nepatiess, neviena darbība netiek pildīta.\n";
 
   pareizAtbildes[1] = "A,C";
+  pareizAtbildes[2] = "A,B,D";
+
+  for(int i=1; i<10; i++){
+    cout<<jautajumi[i]<<"\n";
+    cout<<atbilzuVar[i]<<"\n";
+  }
   
 }
